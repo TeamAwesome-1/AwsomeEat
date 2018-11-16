93198d20286a868074bc1a2946acd174ab78f7fa
TommyAndersson1991 <tommy.andersson@iths.se>
2018-11-16 14:50:18 +0100

    Added FirestoreMain.java - A new class to handle Firestore functionality.

---
+++ app/src/main/java/com/teamawsome/awsomeeat/ResturantList/FirestoreMain.java
@@ -0,0 +1,103 @@
+package com.teamawsome.awsomeeat.ResturantList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.teamawsome.awsomeeat.Firestore;
+import com.teamawsome.awsomeeat.R;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+public class FirestoreMain extends AppCompatActivity {
+    private int count = 0;
+
+    private static final FirestoreMain FirestoreMain = new FirestoreMain();
+
+    public static FirestoreMain getInstance () {
+        return FirestoreMain;
+    }
+
+    private FirestoreMain () {
+
+    }
+
+        //Lägg till ny restaurang.
+    //TODO: Lägg till möjlighet att manuellt skriva in restaurangnamn och adress.
+    //TODO: Fixa bugg som gör att metoden körs i oändlighet.
+        public void addRestaurant (FirebaseFirestore db, RestaurantRecyclerViewAdapter adapter) {
+
+
+            db.collection("restaurants").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                    if (e != null){
+                        return;
+                    }
+
+                    for (DocumentChange dc: queryDocumentSnapshots.getDocumentChanges()){
+                        if (dc.getType() == DocumentChange.Type.ADDED){
+
+                            String id = dc.getDocument().getId();
+                            Restaurant restaurant = dc.getDocument().toObject(Restaurant.class);
+                            restaurant.id = id;
+                            adapter.addItem(restaurant);
+                            count++;
+                            Restaurant r = new Restaurant("test"+ count, "Gamla sjövägen","http://medifoods.my/images/menu/p1_ginger_pao.jpg");
+                            db.collection("restaurants")
+                                    .add(r);
+                            return;
+                        }
+                        else if (dc.getType() == DocumentChange.Type.REMOVED){
+                            String id = dc.getDocument().getId();
+                            adapter.removeResturant(id);
+                        }
+                    }
+
+
+                }
+            });
+
+
+        }
+
+        //Lägger till i vaurkorg
+        public void addToCart () {
+
+        }
+
+        //Hämtar vald restaurangs meny.
+        public void getRestaurantMenu () {
+
+
+        }
+
+        //Söker efter en maträtt
+        public void searchFood () {
+
+
+        }
+
+
+        //Rensar listan med restauranger.
+        public void clearRestaurants (FirebaseFirestore db, RestaurantRecyclerViewAdapter adapter) {
+
+
+
+        }
+
+
+    }
+
+
+
+
--- app/src/main/java/com/teamawsome/awsomeeat/ResturantList/ResturantActivity.java
+++ app/src/main/java/com/teamawsome/awsomeeat/ResturantList/ResturantActivity.java
@@ -1,9 +1,14 @@
 package com.teamawsome.awsomeeat.ResturantList;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.RequiresPermission;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -18,52 +23,40 @@ import java.util.Map;
 
 import javax.annotation.Nullable;
 
-public class ResturantActivity extends AppCompatActivity {
-    List<Restaurant> itemList = new ArrayList<>();
-    Map<String, Restaurant> restaurants = new HashMap<>();
-    private RestaurantRecyclerViewAdapter adapter;
-    FirebaseFirestore db;
-    private int count;
+public class ResturantActivity extends AppCompatActivity implements View.OnClickListener {
+    public List<Restaurant> itemList = new ArrayList<>();
+    public Map<String, Restaurant> restaurants = new HashMap<>();
+    public RestaurantRecyclerViewAdapter adapter;
+    public FirebaseFirestore db;
+    private FirestoreMain firestoreMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resturant);
-        db= FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
         RecyclerView recyclerView = findViewById(R.id.list);
         adapter = new RestaurantRecyclerViewAdapter(this, itemList);
         recyclerView.setAdapter(adapter);
+        firestoreMain = FirestoreMain.getInstance();
+        FloatingActionButton button = findViewById(R.id.Button);
+        button.setOnClickListener(this::onClick);
 
-        db.collection("restaurants").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                if (e != null){
-                    return;
-                }
+    }
 
-                for (DocumentChange dc: queryDocumentSnapshots.getDocumentChanges()){
-                    if (dc.getType() == DocumentChange.Type.ADDED){
+    @Override
+    public void onClick(View view) {
+        firestoreMain.addRestaurant(db, adapter);
+    }
 
-                        String id = dc.getDocument().getId();
-                        Restaurant restaurant = dc.getDocument().toObject(Restaurant.class);
-                        restaurant.id = id;
-                        adapter.addItem(restaurant);
-                    }
-                        else if (dc.getType() == DocumentChange.Type.REMOVED){
-                        String id = dc.getDocument().getId();
-                        adapter.removeResturant(id);
-                    }
-                }
 
-                }
-                });
 
-        findViewById(R.id.Button).setOnClickListener((View view) -> {
-            count++;
-            Restaurant r = new Restaurant("Restaurant "+ count, "Gamla sjövägen","http://medifoods.my/images/menu/p1_ginger_pao.jpg");
 
-            db.collection("restaurants")
-                    .add(r);
+
+
+
+
+
                     /*.addOnCanceledListener(new OnSuccessListener<DocumentReference>() {
                         @Override
                         public void onSuccess(DocumentReference documentReference) {
@@ -73,9 +66,20 @@ public class ResturantActivity extends AppCompatActivity {
                         @Override
                         public void onFailure(@NonNull Exception e) {
                  }});*/
-        });
 
 
-    }
 
+
+
+
+
+        public void clearRestaurants () {
+            findViewById(R.id.removeRestaurantsButton).setOnClickListener((View view) -> {
+                firestoreMain.clearRestaurants(db, adapter);
+            });
+
+        }
 }
+
+
+
