f0fb8aee8b63b0045dc8f51f0ef19b990cf68c1a
robinbruer <r_bruer@hotmail.com>
2018-11-26 13:33:57 +0100

    Automatic loggin

--- app/src/main/java/com/teamawsome/awsomeeat/MainActivity.java
+++ app/src/main/java/com/teamawsome/awsomeeat/MainActivity.java
@@ -2,23 +2,29 @@ package com.teamawsome.awsomeeat;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSignIn,btnSignUp;
     TextView txtSlogan;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setupFirebaseAuth();
 
         btnSignIn=(Button) findViewById(R.id.btnSignIn);
         btnSignUp=(Button) findViewById(R.id.btnSignUp);
@@ -51,4 +57,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
         }
+
+    private void setupFirebaseAuth(){
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Log.d("Login", "onAuthStateChanged: signed_in: " + user.getUid());
+                    Intent intent = new Intent(MainActivity.this, AwsomeEatActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }else{
+                    Log.d("Login", "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
     }
